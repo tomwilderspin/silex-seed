b6e31461e40523983a36033d4ccdceef7044cf24
tomwilderspin <tomwilderspin@gmail.com>
2014-10-29 16:40:26 +0000

    docker up and running!

--- web/index.php
+++ web/index.php
@@ -6,6 +6,10 @@ require_once '../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->get('/', function() use($app) {
+    return 'root path!';
+});
+
 $app->get('/hello/{name}', function($name) use($app) {
     return 'Hello '.$app->escape($name);
 });
